14b9898816d56f6019e68267af0b3233496713d0
5111qz <139861805+5111qz@users.noreply.github.com>
2023-07-26 13:04:27 +0200

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,12 +5,8 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    try {
+  public Student find(long studentID) throws CustomException(studentID) {
     return Student.getValueOf(studentID);
-    } catch (CustomException e) {
-      throw new CustomException(studentID);
-  }
   }
 
   public static void main(String[] args) {
